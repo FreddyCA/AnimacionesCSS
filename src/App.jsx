885c8efbba6d22141367fabb480872d36015203b
Freddy Coaricona <fcoaricona@outlook.com>
2024-05-22 23:19:00 -0400

    Add UI StackingCards

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import GaleriaRyM from "./components/GaleriaRyM";
 import InfoPeter from "./components/InfoPeter";
 import NavBar from "./components/NavBar";
 import Progress from "./components/Progress";
+import StackingCards from "./components/StackingCards";
+import StackingCardsV2 from "./components/StackingCardsV2";
 import GlobalStyle from "./style/reset";
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
-      <NavBar />
-      <Progress />
-      <InfoPeter />
-      <GaleriaRyM />
+      {/* <NavBar /> */}
+      {/* <Progress /> */}
+      {/* <InfoPeter /> */}
+      {/* <GaleriaRyM /> */}
+      {/* <StackingCards /> */}
+      <StackingCardsV2 />
     </>
   );
 };
---
+++ src/components/StackingCardsV2.jsx
@@ -0,0 +1,121 @@
+import "../style/stackingCardsV2.css";
+import imganesF from "../img/img-1.jpg"
+const StackingCardsV2 = () => {
+  return (
+    <>
+      <header>
+        <div>
+          <h1>Stacking Cards</h1>
+          <p>Scroll down to see the effect</p>
+        </div>
+      </header>
+      <main>
+        <ul id="cards">
+          <li className="card" id="card_1">
+            <div className="card__content">
+              <div>
+                <h2>Card One</h2>
+                <p>aaaaaaaaaaa aaaaaa</p>
+                <p>Read more</p>
+              </div>
+              <figure>
+                <img src={imganesF} alt="image"/>
+              </figure>
+            </div>
+          </li>
+          <li className="card" id="card_2">
+            <div className="card__content">
+              <div>
+                <h2>Card One</h2>
+                <p>aaaaaaaaaaa aaaaaa</p>
+                <p>Read more</p>
+              </div>
+              <figure>
+                <img src={imganesF} alt="image"/>
+              </figure>
+            </div>
+          </li>
+          <li className="card" id="card_3">
+            <div className="card__content">
+              <div>
+                <h2>Card One</h2>
+                <p>aaaaaaaaaaa aaaaaa</p>
+                <p>Read more</p>
+              </div>
+              <figure>
+                <img src={imganesF} alt="image"/>
+              </figure>
+            </div>
+          </li>
+          <li className="card" id="card_4">
+            <div className="card__content">
+              <div>
+                <h2>Card One</h2>
+                <p>aaaaaaaaaaa aaaaaa</p>
+                <p>Read more</p>
+              </div>
+              <figure>
+                <img src={imganesF} alt="image"/>
+              </figure>
+            </div>
+          </li>
+        </ul>
+      </main>
+
+      <aside>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem repellat
+          non ratione eum provident quaerat obcaecati dicta autem voluptates sit
+          cum quis distinctio, atque sint commodi reprehenderit perspiciatis
+          iure velit molestiae eos molestias ipsa nihil quia? Porro tempore in
+          fugit, aspernatur et temporibus aliquam magnam adipisci deleniti dicta
+          repellat alias nostrum impedit quidem odit excepturi nam. Itaque
+          numquam unde quae dignissimos exercitationem placeat, ipsa amet
+          tempora vitae ullam animi! Hic asperiores in esse atque expedita
+          dolorem officia illo commodi fugit earum ab, assumenda optio, sed
+          repellat. Maiores laudantium reprehenderit suscipit accusamus
+          assumenda, debitis nemo ut sapiente numquam doloremque alias explicabo
+          minus autem harum illum dicta. Unde obcaecati inventore repellat id
+          eligendi eaque, rerum aliquam maiores aspernatur cumque tenetur quo
+          qui mollitia atque quasi voluptate ipsum neque explicabo voluptatem
+          quibusdam beatae quae iste vero doloribus! Natus libero quibusdam
+          molestias harum atque, assumenda doloribus quis, quam corporis,
+          voluptas suscipit magni! Enim sapiente totam consequatur repellat
+          maxime placeat perferendis iste maiores soluta, obcaecati incidunt
+          expedita eius cumque ipsa quo sed. Minus distinctio culpa cumque
+          labore nulla laborum ex, explicabo magni mollitia eaque aliquam vero
+          blanditiis quae, id necessitatibus error voluptas harum tempora sint
+          porro. Facilis beatae quasi soluta et ipsum suscipit repellendus
+          inventore.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem repellat
+          non ratione eum provident quaerat obcaecati dicta autem voluptates sit
+          cum quis distinctio, atque sint commodi reprehenderit perspiciatis
+          iure velit molestiae eos molestias ipsa nihil quia? Porro tempore in
+          fugit, aspernatur et temporibus aliquam magnam adipisci deleniti dicta
+          repellat alias nostrum impedit quidem odit excepturi nam. Itaque
+          numquam unde quae dignissimos exercitationem placeat, ipsa amet
+          tempora vitae ullam animi! Hic asperiores in esse atque expedita
+          dolorem officia illo commodi fugit earum ab, assumenda optio, sed
+          repellat. Maiores laudantium reprehenderit suscipit accusamus
+          assumenda, debitis nemo ut sapiente numquam doloremque alias explicabo
+          minus autem harum illum dicta. Unde obcaecati inventore repellat id
+          eligendi eaque, rerum aliquam maiores aspernatur cumque tenetur quo
+          qui mollitia atque quasi voluptate ipsum neque explicabo voluptatem
+          quibusdam beatae quae iste vero doloribus! Natus libero quibusdam
+          molestias harum atque, assumenda doloribus quis, quam corporis,
+          voluptas suscipit magni! Enim sapiente totam consequatur repellat
+          maxime placeat perferendis iste maiores soluta, obcaecati incidunt
+          expedita eius cumque ipsa quo sed. Minus distinctio culpa cumque
+          labore nulla laborum ex, explicabo magni mollitia eaque aliquam vero
+          blanditiis quae, id necessitatibus error voluptas harum tempora sint
+          porro. Facilis beatae quasi soluta et ipsum suscipit repellendus
+          inventore.
+        </p>
+      </aside>
+    </>
+  );
+};
+
+export default StackingCardsV2;
